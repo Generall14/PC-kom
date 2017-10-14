6f33a090e18e9bc33da0aecae372197433110085
Wojciech Kogut <wojciech_kogut@o2.pl>
2017-10-14 22:35:37 +0200

    Wsparcie dla Doxygen.

--- src/MediumUI.hpp
+++ src/MediumUI.hpp
@@ -1,6 +1,22 @@
 #ifndef MEDIUMUI_HPP
 #define MEDIUMUI_HPP
 
+/**
+ * \class MediumUI
+ * @brief Interfejs użytkownika do wyboru medium transmisyjnego.
+ *
+ * Pobiera w konstruktorze a nastepnie przetrzymuje w zmiennej cParent wskaźnik na obiekt QFrame na którym będzie budowany interjejs graficzny. Inicjalizacja interfejsu
+ * powinna nastapić w metodzie wirtualnej Init().
+ *
+ * Na sloty Connected() oraz Disconnected() przesyłane są sygnały od klasy Mendium informujące o stanie połączenia. Interfejs powinien odpowiednio na nie zareagować poprzez
+ * wygaszenie odpowiednich opcji.
+ *
+ * Klasa zależnie od poczynań użytkownika powinna wysyłać sygnały DisconnectRequest() oraz ConnectRequest(QString) w celu zarządzania połączeniem w klasie Mendium. Argument w sygnale
+ * ConnectRequest(QString) powinien opisywać parametry połączenia zgodnie z wymaganiami danej implementacji interfejsu Mendium.
+ *
+ * Sygnalizacja wszelkich błędów odbywa się za pomocą sygnału Error(QString).
+ */
+
 #include <QFrame>
 #include <QString>
 #include <QObject>
@@ -12,19 +28,19 @@ public:
     MediumUI(QFrame* parent);
     virtual ~MediumUI(){}
 
-    virtual void Init() = 0;
+    virtual void Init() = 0;                                                            /**<Inicjalizacja interfejsu użytkownika*/
 
 public slots:
-    virtual void Connected() = 0;
-    virtual void Disconnected() = 0;
+    virtual void Connected() = 0;                                                       /**<Reakcja na połączenie z medium*/
+    virtual void Disconnected() = 0;                                                    /**<Reakcja na rozłączenie z medium*/
 
 signals:
-    void ConnectRequest(QString desc);
-    void DisconnectRequest();
-    void Error(QString msg);
+    void ConnectRequest(QString desc);                                                  /**<Żądanie połączenia z medium*/
+    void DisconnectRequest();                                                           /**<Żądanie rozłączenia z medium*/
+    void Error(QString msg);                                                            /**<Informowanie o błędzie*/
 
 protected:
-    QFrame* cParent=NULL;
+    QFrame* cParent=NULL;                                                               /**<Wskaźnik na podstawę interfejsu użytkownika*/
 };
 
 #endif
--- src/Mendium.hpp
+++ src/Mendium.hpp
@@ -1,6 +1,25 @@
 #ifndef MENDIUM_HPP
 #define MENDIUM_HPP
 
+/**
+ * \class Mendium
+ * @brief Interfejs klas obsługujących medium transmisyjne.
+ *
+ * Klasa działa w osobnym wątku (na zasadach dziedziczonych po EQThread).
+ *
+ * Implementuje zwracanie logicznego stanu opened w metodzie isOpened() oraz metodę Stop() dziedziczoną po EQThread. Pozostałe metody są czysto wirtualne.
+ *
+ * Sterowanie stanem medium zapewniają sloty Open(QString) oraz Close(), pobudzanę są sygnałami z klas MediumUI. Zmiana stanu powinna być zaznaczona rozesłaniem sygnału Opened()
+ * bądź Closed().
+ *
+ * Slot Write(QSharedPointer<Frame>) obsługuje zapis danych do medium transmisyjnego, sygnały na ten slot pochodzą z klasy FrameBuilder (gdzie zostały wymuszone przez LogicUI).
+ * Wszystkie odczytane dane z medium powinny być rozesłane przez sygnał Readed(QByteArray) (trafią do FrameBuilder).
+ *
+ * Metoda Flush() powinna opróżniać wszelkie bufory zapisu i odczytu (jeżeli takowe istnieją).
+ *
+ * Sygnalizacja wszelkich błędów odbywa się za pomocą sygnału Error(QString).
+ */
+
 #include <QObject>
 #include "EQThread.hpp"
 #include <QByteArray>
@@ -15,22 +34,22 @@ public:
     virtual ~Mendium(){}
 
 public slots:
-    bool isOpened();
-    virtual void Stop();
-    virtual void Open(QString desc) = 0;
-    virtual void Close() = 0;
-    virtual void Write(QSharedPointer<Frame> frame) = 0;
-    virtual void Flush() = 0;
+    bool isOpened();                                                                    /**<Czy medium jest otwarte*/
+    virtual void Stop();                                                                /**<Zatrzymaj wątek*/
+    virtual void Open(QString desc) = 0;                                                /**<Otwórz medium opisane tekstem desc*/
+    virtual void Close() = 0;                                                           /**<Zamknij medium*/
+    virtual void Write(QSharedPointer<Frame> frame) = 0;                                /**<Wyslij ramkę do medium*/
+    virtual void Flush() = 0;                                                           /**<Opróżnij bufory*/
 
 protected:
     virtual void Run() = 0;
     bool opened = false;
 
 signals:
-    void Opened();
-    void Closed();
-    void Readed(QByteArray);
-    void Error(QString);
+    void Opened();                                                                      /**<Otwarto medium*/
+    void Closed();                                                                      /**<Zamknięto medium*/
+    void Readed(QByteArray);                                                            /**<Odczytano dane z medium*/
+    void Error(QString);                                                                /**<Komunikacja błędu*/
 };
 
 #endif
